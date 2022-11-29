92c311c6eb8085db2822938e92cefed69183f066
Mayuribeldar <50705335+Mayuribeldar@users.noreply.github.com>
2022-11-29 18:01:03 +0530

    Add files via upload

---
+++ todo.cy.js
@@ -0,0 +1,22 @@
+describe('mayuri 1st test suite',function()
+{
+it("mayuri test case no 01 ",function(){
+cy. visit("https://shopnewage.com")
+
+})
+
+
+it("mayuri test case no 02 ",function()
+{
+  cy. visit("https://newageproducts.com/") 
+})
+
+
+it("mayuri test case no 03 ",function()
+{
+test
+})
+
+
+}
+)
